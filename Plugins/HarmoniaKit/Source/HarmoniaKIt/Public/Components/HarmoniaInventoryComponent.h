4ed33364a3fb2a7168c64c6c4ad8c0055c185c56
SH <73706510+RedFlowering@users.noreply.github.com>
2025-07-23 21:41:33 +0900

    인벤토리 서버 동기화 코드 추가, 리프레시 델리게이트 추가

--- Plugins/HarmoniaKit/Source/HarmoniaKIt/Public/Components/HarmoniaInventoryComponent.h
+++ Plugins/HarmoniaKit/Source/HarmoniaKIt/Public/Components/HarmoniaInventoryComponent.h
@@ -12,55 +12,60 @@ class AHarmoniaItemActor;
 // 드랍 이벤트 델리게이트
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryChanged);
 
-UCLASS(ClassGroup=(Harmonia), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Harmonia), meta = (BlueprintSpawnableComponent))
 class HARMONIAKIT_API UHarmoniaInventoryComponent : public UActorComponent
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    UHarmoniaInventoryComponent();
+	UHarmoniaInventoryComponent();
 
 protected:
-    virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 public:
-    UFUNCTION(BlueprintCallable, Category="Inventory")
-    void RequestPickupItem(AHarmoniaItemActor* Item);
+	UFUNCTION()
+	void OnRep_InventoryData();
 
-    UFUNCTION(BlueprintCallable, Category="Inventory")
-    void RequestDropItem(int32 SlotIndex);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void RequestPickupItem(AHarmoniaItemActor* Item);
 
-    UFUNCTION(BlueprintCallable, Category="Inventory")
-    bool AddItem(const FHarmoniaID& ItemID, int32 Count, float Durabiliry);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void RequestDropItem(int32 SlotIndex);
 
-    UFUNCTION(BlueprintCallable, Category="Inventory")
-    bool RemoveItem(const FHarmoniaID& ItemID, int32 Count, float Durabiliry);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	bool AddItem(const FHarmoniaID& ItemID, int32 Count, float Durability);
 
-    UFUNCTION(BlueprintCallable)
-    void SwapSlots(int32 SlotA, int32 SlotB);
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	bool RemoveItem(const FHarmoniaID& ItemID, int32 Count, float Durability);
 
-    UFUNCTION(BlueprintCallable, Category="Inventory")
-    void Clear();
+	UFUNCTION(BlueprintCallable)
+	void SwapSlots(int32 SlotA, int32 SlotB);
 
-    UFUNCTION(BlueprintCallable, Category="Inventory")
-    int32 GetTotalCount(const FHarmoniaID& ItemID) const;
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void Clear();
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	int32 GetTotalCount(const FHarmoniaID& ItemID) const;
 
 protected:
-    void PickupItem(AHarmoniaItemActor* Item);
+	void PickupItem(AHarmoniaItemActor* Item);
 
 	UFUNCTION(Server, Reliable)
-    void ServerPickupItem(AHarmoniaItemActor* Item);
+	void ServerPickupItem(AHarmoniaItemActor* Item);
 
-    void DropItem(int32 SlotIndex);
+	void DropItem(int32 SlotIndex);
 
-    UFUNCTION(Server, Reliable)
-    void ServerDropItem(int32 SlotIndex);
+	UFUNCTION(Server, Reliable)
+	void ServerDropItem(int32 SlotIndex);
 
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Inventory")
-    FInventoryData InventoryData = FInventoryData();
+	// 서버 <-> 오너 클라만 동기화
+	UPROPERTY(ReplicatedUsing = OnRep_InventoryData, EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	FInventoryData InventoryData = FInventoryData();
 
-    // UI/위젯에서 바인딩할 수 있는 변경 이벤트
-    UPROPERTY(BlueprintAssignable)
-    FOnInventoryChanged OnInventoryChanged;
+	// 클라 UI에서 바인딩하는 변경 이벤트
+	UPROPERTY(BlueprintAssignable)
+	FOnInventoryChanged OnInventoryChanged;
 };
